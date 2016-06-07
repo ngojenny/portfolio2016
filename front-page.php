157870ec7400a3fbe74b3fe3c464cac94af213aa
Jenny Ngo <jennyngo.dev@gmail.com>
2016-06-07 14:30:19 -0400

    created basic html structure

--- front-page.php
+++ front-page.php
@@ -6,11 +6,43 @@
 
 get_header();  ?>
 
+<!-- HEADER STARTS-->
+<header id="home">
+	<div class="wrapper">
+		<h1>Jenny Ngo</h1>
+		<h2>Front-End Developer</h2>
+	</div> <!-- /.wrapper -->
+</header><!--/.header-->
 
+<!-- ABOUT SECTION STARTS -->
+<section class="about" id="about">
+	<div class="wrapper">
 		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		  <h2>About</h2>
-		  <?php the_content(); ?>
-
+			<h2>About</h2>
+			<?php the_content(); ?>
 		<?php endwhile; // end the loop?>
+	</div> <!-- /.wrapper -->
+</section>
+
+<!-- WORK SECTION STARTS -->
+<section class="work" id="work">
+	<div class="wrapper">
+		<h2>Work</h2>
+	</div> <!-- /.wrapper -->
+</section>
+
+<!-- SKILLS SECTION STARTS -->
+<section class="skills" id="skills">
+	<div class="wrapper">
+		<h2>Skills</h2>
+	</div> <!-- /.wrapper -->
+</section>
+
+<!-- CONTACT SECTION STARTS -->
+<section class="contact" id="contact">
+	<div class="wrapper">
+		<h2>Contact</h2>
+	</div> <!-- /.wrapper -->
+</section>
 	
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -13,14 +13,26 @@
 
 <body <?php body_class(); ?>>
 
-<header>
-	<div class="container">
-		<h1>Jenny Ngo</h1>
-		<h2>Front-End Developer</h2>
-		<?php wp_nav_menu( array(
-			'container' => false,
-			'theme_location' => 'primary'
-		)); ?>
-	</div> <!-- /.container -->
-</header><!--/.header-->
+<!-- NAVIGATION STARTS-->
+<nav>
+	<div class="wrapper">
+		<ul class="main-nav">
+			<li>
+				<a href="#home">Home</a>
+			</li>
+			<li>
+				<a href="#about">About</a>
+			</li>
+			<li>
+				<a href="#work">Work</a>
+			</li>
+			<li>
+				<a href="#skills">Skills</a>
+			</li>
+			<li>
+				<a href="#contact">Contact</a>
+			</li>
+		</ul>
+	</div> <!-- /.wrapper -->
+</nav>
 
